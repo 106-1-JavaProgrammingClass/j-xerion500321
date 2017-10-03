fd0e0d0b90b3934e1ccc7d830fbe3504f99be6c6
xerion500321 <32055771+xerion500321@users.noreply.github.com>
2017-10-03 23:59:21 +0800

    j-xerion500321

--- HW14.java
+++ HW14.java
@@ -4,7 +4,7 @@ public class HW14 {
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-
-
+        char word = scn.next().charAt(0);
+        System.out.println((Integer.toHexString(word)));
     }
 }
--- HW15.java
+++ HW15.java
@@ -4,7 +4,8 @@ public class HW15 {
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-
-
+        String  a = scn.next();
+        int b = Integer.valueOf(a,16);
+        System.out.println((char)b);
     }
     }
